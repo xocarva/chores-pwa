6d85926ad3090bc7404bfc1b6478a727b12739c8
xocarva <correo@xoancarneiro.com>
2024-02-24 18:50:04 +0100

    add useAuth hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,25 @@
+import { useAuthStore } from '../stores';
+import { api } from '../api';
+
+export const useAuth = () => {
+  const login = async (email: string, password: string) => {
+    try {
+      const { data } = await api.post('/login', {
+        email,
+        password,
+      });
+      localStorage.setItem('token', data.token);
+      useAuthStore.getState().login(data.userName);
+    } catch (error) {
+      console.error('Login error', error);
+      throw error;
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    useAuthStore.getState().logout();
+  };
+
+  return { login, logout };
+};
