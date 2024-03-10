c40228475178fb610e924f7be0f0fe0a2d28599a
xocarva <correo@xoancarneiro.com>
2024-03-10 13:45:04 +0100

    change task card styles

--- src/tasks/components/TaskCard.tsx
+++ src/tasks/components/TaskCard.tsx
@@ -7,7 +7,13 @@ import {
   IconButton,
   Typography,
   Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  AvatarGroup,
+  Box,
 } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -23,14 +29,61 @@ interface TaskCardProps {
 function TaskCard({ task, onEdit, onDelete, onComplete }: TaskCardProps) {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} key={task.id}>
-      <Card>
-        <CardHeader
-          avatar={task.users.map((user) => (
-            <Avatar key={user.id} title={user.name}>
-              {user.name[0].toUpperCase()}
-            </Avatar>
-          ))}
-          action={
+      <Card
+        key={task.id}
+        sx={{
+          border: 1, // Establece el ancho del borde
+          borderColor: 'primary.main', // Utiliza el color primary del tema para el borde
+          ':hover': {
+            boxShadow: 6, // Opcional: Aumenta la sombra al pasar el mouse para un efecto visual
+          },
+        }}
+      >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={task.description && <ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <CardHeader
+              sx={{ minHeight: '100px' }}
+              title={task.title}
+              subheader={
+                task.date
+                  ? `Límite: ${new Date(task.date).toLocaleDateString()}`
+                  : 'Sen data límite'
+              }
+              titleTypographyProps={{
+                variant: 'h6',
+              }}
+            />
+          </AccordionSummary>
+          {task.description && (
+            <AccordionDetails>
+              <CardContent>
+                <Typography fontStyle="italic">
+                  &quot;{task.description}&quot;
+                </Typography>
+              </CardContent>
+            </AccordionDetails>
+          )}
+        </Accordion>
+        <CardActions sx={{ justifyContent: 'space-between' }}>
+          <AvatarGroup max={3}>
+            {task.users.map((user) => (
+              <Avatar key={user.id} title={user.name}>
+                {user.name[0].toUpperCase()}
+              </Avatar>
+            ))}
+          </AvatarGroup>
+          <Box>
+            <IconButton
+              aria-label="delete"
+              title="eliminar"
+              onClick={() => onDelete(task.id)}
+            >
+              <DeleteIcon />
+            </IconButton>
             <IconButton
               aria-label="settings"
               title="editar"
@@ -38,35 +91,14 @@ function TaskCard({ task, onEdit, onDelete, onComplete }: TaskCardProps) {
             >
               <EditIcon />
             </IconButton>
-          }
-          title={task.title}
-          subheader={`Límite: ${
-            task.date
-              ? new Date(task.date).toLocaleDateString()
-              : 'No especificada'
-          }`}
-          titleTypographyProps={{ variant: 'h5' }}
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            Usuarios asignados: {task.users.map((user) => user.name).join(', ')}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton
-            aria-label="complete"
-            title="marcar como completada"
-            onClick={() => onComplete(task.id)}
-          >
-            <CheckCircleOutlineIcon />
-          </IconButton>
-          <IconButton
-            aria-label="delete"
-            title="eliminar"
-            onClick={() => onDelete(task.id)}
-          >
-            <DeleteIcon />
-          </IconButton>
+            <IconButton
+              aria-label="complete"
+              title="marcar como completada"
+              onClick={() => onComplete(task.id)}
+            >
+              <CheckCircleOutlineIcon fontSize="large" />
+            </IconButton>
+          </Box>
         </CardActions>
       </Card>
     </Grid>
