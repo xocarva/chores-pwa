de090abe7284405eaa09123bd9f78ea60a2ff1c3
xocarva <correo@xoancarneiro.com>
2024-03-15 16:35:35 +0100

    fix create invitation request

--- src/invitations/api/adapters/axiosResponseToCreateInvitation.ts
+++ src/invitations/api/adapters/axiosResponseToCreateInvitation.ts
@@ -1,7 +1,7 @@
 import { AxiosResponse } from 'axios';
 
 const axiosResponseToCreateInvitation = (res: AxiosResponse): string => {
-  const url = import.meta.env.VITE_API_BASE_URL;
+  const url = import.meta.env.VITE_APP_URL;
   const { token } = res.data;
 
   return `${url}/${token}`;
--- src/invitations/api/requests/createInvitation.request.ts
+++ src/invitations/api/requests/createInvitation.request.ts
@@ -3,9 +3,8 @@ import { api, axiosErrorToCustomError } from '../../../core';
 import { axiosResponseToCreateInvitation } from '../adapters';
 
 const createInvitation = async (spaceId: number): Promise<string> => {
-  return 'https://chores.com/invitation/dsh32u432fdb34';
   return api
-    .post('/invitation', { spaceId })
+    .post('/invitations', { spaceId })
     .then((res: AxiosResponse) => axiosResponseToCreateInvitation(res))
     .catch((error: AxiosError) =>
       Promise.reject(axiosErrorToCustomError(error))
